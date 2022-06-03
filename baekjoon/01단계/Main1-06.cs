ea3922dcaae9f81050aaba14e8104610034ed81c
Kalra123 <tigar3409@naver.com>
2022-06-03 17:23:30 +0900

    백준

---
+++ baekjoon/01단계/Main1-06.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baekjoon._01단계
+{
+    internal class Main1_06
+    {
+        static void Main1(string[] args)
+        {
+            string[] num = Console.ReadLine().Split();
+
+            
+            
+        }
+    }
+}
---
+++ baekjoon/01단계/Main1-10.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace baekjoon._01단계
+{
+    internal class Main1_10
+    {
+        static void Main(string[] args)
+        {
+            string greeting = "beakjoon";
+            Console.ReadLine();
+            Console.WriteLine("beakjoon");
+            Console.WriteLine("substring(0,8) : {0}", greeting.Substring(0, 8));
+            string greeting1 = "baekjoon??!";
+            Console.ReadLine();
+            Console.WriteLine("baekjoon??!");
+            Console.WriteLine("substring(0,11) : {0}", greeting1.Substring(0, 11));
+        }
+        
+    }
+}
