be3ec5ad5c5bf64d400148f205eab13a6d461d86
Kalra123 <tigar3409@naver.com>
2022-06-03 12:19:40 +0900

    2022/06/03 수업예제

--- ch03/1_if.cs
+++ ch03/1_if.cs
@@ -3,6 +3,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+/*
+ * 날짜 2022/06/03
+ * 이름 조진호
+ * 내용 조건문 실습 교재 p127
+ */
 
 namespace ch03
 {
@@ -10,7 +15,77 @@ namespace ch03
     {
         static void Main1(string[] args)
         {
+            // if
+            int num1 = 1;
+            int num2 = 2;
 
+            if(num1 < num2)
+            {
+                // 조건이 참이면 실행, 거짓이면 건너뜀
+                Console.WriteLine("num1은 num2보다 작다");
+            }
+            Console.WriteLine();
+
+
+            if (num1>1)
+            {
+                Console.WriteLine("num1은 1보다 크다.");
+
+            }
+            Console.WriteLine();
+
+            if(num1>0)
+            {
+                if(num2>1)
+                {
+                    Console.WriteLine("num1은 0보다 크고, 1보다 작다");
+                }
+            }
+            if(num1>0 && num2 > 1)
+            {
+                Console.WriteLine("num1은 0보다 크고 그리고 num2는 1보다 크다.");
+            }
+            Console.WriteLine();
+
+            // if ~ else
+            int var1 = 1, var2 = 2;
+            
+            if(var1 > var2)
+            {
+                //조건이 참이면
+                Console.WriteLine("var1은 var2보다 크다.");
+            }
+            else
+            {
+                //조건이 거짓이면
+                Console.WriteLine("var1은 var2보다 작다.");
+            }
+            Console.WriteLine();
+
+            // if ~ else if ~ else
+            int n1 = 10, n2 = 20, n3 = 30, n4 = 40;
+
+            if(n1 > n2)
+                {
+                Console.WriteLine("n1은 n2보다 크다");
+
+            }
+            else if (n2 > n3)
+            {
+                Console.WriteLine("n2은 n3보다 크다");
+
+            }
+            else if (n3 > n4)
+            {
+                Console.WriteLine("n3은 n4보다 크다");
+
+            }
+            else
+            {
+                Console.WriteLine("n4가 가장 크다");
+
+            }
+        
         }
     }
 }
--- ch03/2_switch.cs
+++ ch03/2_switch.cs
@@ -3,14 +3,45 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
+/*
+ * 날짜 2022/06/03
+ * 이름 조진호
+ * 내용 조건문 실습 교재 p127
+ */
 namespace ch03
 {
     internal class _switch
     {
-        static void Main1(string[] args)
+        static void Main(string[] args)
         {
+            Console.Write("점수 입력 : ");
+            string strscore = Console.ReadLine();
 
+            int score = int.Parse(strscore);
+            int grade = score / 10;
+
+            switch (grade)
+            {
+                case 10:
+                    Console.WriteLine("A+ 입니다.");
+                    break;
+                case 9:
+                    Console.WriteLine("A 입니다.");
+                    break;
+                case 8:
+                    Console.WriteLine("B 입니다.");
+                    break;
+                case 7:
+                    Console.WriteLine("C 입니다.");
+                    break;
+                case 6:
+                    Console.WriteLine("D 입니다.");
+                    break;
+                default:
+                    Console.WriteLine("F 입니다.");
+                    break;
+            }
+               
         }
     }
 }
