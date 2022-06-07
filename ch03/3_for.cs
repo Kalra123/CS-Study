6bb0ee9e16db14487fd00ea7fffafed3ffaba3cc
Kalra123 <tigar3409@naver.com>
2022-06-07 17:20:20 +0900

    챕터 3장

--- ch03/3_for.cs
+++ ch03/3_for.cs
@@ -10,7 +10,26 @@ namespace ch03
     {
         static void Main1(string[] args)
         {
+            // for문 = for (초기식, 조건식, 증감식)
+            for (int i = 1; i <= 5; i++)
+            {
+                Console.WriteLine("(0)회를 반복...", i);
+            }
 
+            // 1부타 10까지 합
+            int sum = 0;
+
+            for (int k = 1; k <= 10; k++)
+            {
+                sum += k;
+
+            }
+            Console.WriteLine("1부터 10까지의 합 : " + sum);
+
+            // 부터 10까지 짝수합
         }
+
+            
+        
     }
 }
