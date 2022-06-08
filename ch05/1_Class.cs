70e7c95c354d2cfea7659a1594bcd45b0f3ac3de
Kalra123 <tigar3409@naver.com>
2022-06-08 10:59:21 +0900

    5장

---
+++ ch05/1_Class.cs
@@ -0,0 +1,61 @@
+﻿using ch05.sub1; //ctrl + .
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * 날짜 2022/06/08
+ * 이름 조진호
+ * 내용 클래스와 객체 실습하기 교제 p203
+ * 
+ * 클래스와 객체 
+ * - 클래스는 객체를 생성하는 구조체이며, 필드와 메서드로 구성된다.
+ * - 객체는 클래스의 실제 인스턴스이며, new 연산자로 생성한다.
+ * 
+ */
+namespace ch05
+{
+    internal class _1_Class
+    {
+        public static object BWM { get; private set; }
+
+        static void Main(string[] args)
+        {
+            // 객체 생성
+            Car sonata = new Car();
+
+            // 객체 초기화
+            sonata.Name = "소나타";
+            sonata.Color = "흰색";
+            sonata.Speed = 0;
+
+            // 객체 기능
+            sonata.SpeedUp(10);
+            sonata.SpeedUp(50);
+            sonata.SpeedDown(20);
+            sonata.Show();
+
+            // 객체 생성 (BMW, 검정색)
+            BMW M340i = new BMW();
+
+            BMW.BMWName = "BMW 340i";
+            BMW.BMWColor = "Green";
+            BMW.BMWSpeed = 0;
+
+            // Account 객체생성
+            Account kb = new Account();
+            kb.Bank = "국민은행"
+            kb.Id = "101-12-1234";
+            kb.Name = "김유신"
+            kb.Balance = 10000;
+
+            //기능
+            kb.Deposit = (25000);
+            kb.Withdraw = (5000);
+            kb.Show();
+
+
+        }
+    }
+}
---
+++ ch05/sub1/Car.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch05.sub1
+{
+    internal class car      //car 라는 그룹, 클래스 하위 매개변수와 메서드 총합 6개
+    {
+        // 속성(필드)
+        public string color;   //멤버변수, 매개변수라는 필드를 만들어놓음
+        public int speed;       //접근 권한자 public
+        public string name;
+
+
+        // 기능 (메서드)
+        public void speedup(int speed)      //여기서는 메서드의 매개변수 언더바를 통해 구분지을수도 있음
+        {
+            this.speed += speed;       //this.를 붙임으로써 매개변수 언더바보다 더 직관적으로 보이게끔 만든다.
+        }       //' 선언' 을 함으로써, 기능을 만들어놓음
+        public void speeddown() 
+        {
+            this.speed -= speed;
+        }     //멤버 메서드
+        public void show () 
+        {
+            Console.WriteLine("===================");
+            Console.WriteLine("차량명 :" +name);
+            Console.WriteLine("차량 색깔 : "+color);
+            Console.WriteLine("챠량 속도 : "+speed);
+        }
+    }
+}
