81f23cc05b5bcf1794fc100ecb8bcbda0caa542b
Kalra123 <tigar3409@naver.com>
2022-06-10 13:26:30 +0900

    시험2

---
+++ test2/2_04.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test2
+{
+    internal class _2_04
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = { 4, 2, 1, 5, 3 };
+
+            for (int i =0; i<4; i++)
+            {
+                for (int j = i +1; j < 5; j++)
+                {
+                    if (arr[i] > arr[j])
+                    {
+                        int temp = arr[i];
+                        arr[j] = arr[i];
+                        arr[i] = temp;
+                    }
+                }
+            }
+            // 정렬된 배열 출력하기
+            foreach (int n in arr)
+            {
+                Console.Write(n + " ");
+            }
+        }
+                
+    }
+}
---
+++ test2/2_10.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test2
+{
+    internal class _2_10
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i< 10; i++)
+            {
+                Console.Write(fibo(1) + " ");
+            }
+        }
+
+        public static int fibo(int n)
+        {
+            if (n <= 1)
+            {
+                return n;
+            }
+            return fibo(n - 1) + fibo(n - 2);
+        }
+    }
+}
